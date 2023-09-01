b762de09bfc6ec4dcbd057749a9ec6bc6dbe844e
prabhavathi2501 <prabhashok2501@gmail.com>
2023-09-01 18:22:28 +0530

    call back,-hell

---
+++ scripts.js
@@ -0,0 +1,127 @@
+//! normal console
+//console.log("first");
+//setTimeout(()=>console.log("secend"),5000);
+//console.log("thirs")
+
+
+//callback using settimeout
+//console.log("app is started");
+//  setTimeout(()=>console.log("loadig....."),2000);
+//  setTimeout(()=>console.log("app completed"),5000);
+// console.log("under procesing");
+
+//call back using
+
+// 
+
+/*setTimeout(() => {
+    console.log("10");
+    document.getElementID("display").innerText = "10";
+
+  setTimeout(() => {
+    console.log("9");
+    document.getElementById("display").innerText ="9";
+    
+    setTimeout(() => {
+        console.log("8");
+        document.getElementById("display").innerText = "8";
+        
+        setTimeout(() => {
+            console.log("7");
+            document.getElementById("display").innerText = "7";
+
+            setTimeout(() => {
+                console.log("6");
+                document.getElementById("display").innerText = "6";
+   
+                setTimeout(() => {
+                    console.log("5");
+                    document.getElementById("display").innerText = "5";
+                   
+                    setTimeout(() => {
+                        console.log("4");
+                        document.getElementById("display").innerText = "4"
+                       
+                        setTimeout(() => {
+                            console.log("3");
+                            document.getElementById("display").innerText = "3";
+
+                            setTimeout(() => {
+                                console.log("2");
+                                document.getElementById("display").innerText = "2";
+
+                                setTimeout(() => {
+                                    console.log("1");
+                                    document.getElementById("display").innerText = "1"
+
+                                    setTimeout(() => {
+                                        document.getElementById("display").innerText = "happy raydhpathan";
+
+                                    },1000);
+
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+
+  }, 1000);*/
+
+//============================================
+//  sir work out this only work
+ /* setTimeout(() => {
+  console.log("10");
+  document.getElementById("display").innerText = "10";
+
+  setTimeout(() => {
+    console.log("9");
+    document.getElementById("display").innerText = "9";
+
+    setTimeout(() => {
+      console.log("8");
+      document.getElementById("display").innerText = "8";
+
+      setTimeout(() => {
+        console.log("7");
+        document.getElementById("display").innerText = "7";
+
+        setTimeout(() => {
+          console.log("6");
+          document.getElementById("display").innerText = "6";
+
+          setTimeout(() => {
+            console.log("5");
+            document.getElementById("display").innerText = "5";
+
+            setTimeout(() => {
+              console.log("4");
+              document.getElementById("display").innerText = "4";
+
+              setTimeout(() => {
+                console.log("3");
+                document.getElementById("display").innerText = "3";
+                setTimeout(() => {
+                  console.log("2");
+                  document.getElementById("display").innerText = "2";
+                  setTimeout(() => {
+                    console.log("1");
+                    document.getElementById("display").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("display").innerText =
+                        "Happy Raksha Bandan ✨ happy celebration to all  ";
+                    },1000);//wish
+                  }, 1000);//1
+                }, 1000);//2
+              }, 1000);//3
+            }, 1000); //4
+          }, 1000); //5
+        }, 1000); //6
+      }, 1000);//7
+    }, 1000); //8
+  }, 1000); //9
+}, 1000);  //10*/
